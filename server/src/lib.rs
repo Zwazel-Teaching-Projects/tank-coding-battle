823dc85fd9bf8d9b684c0f1fc6973c9f15d46a6c
Tim Irmler <zwazel@hotmail.com>
2025-02-28 11:13:38 +0100

    window titles

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -14,7 +14,13 @@ impl Plugin for MyServerPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
             #[cfg(feature = "debug")]
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Server".to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
             #[cfg(not(feature = "debug"))]
             DefaultPlugins.set(bevy::app::ScheduleRunnerPlugin::run_loop(
                 std::time::Duration::from_secs_f64(1.0 / 60.0),
