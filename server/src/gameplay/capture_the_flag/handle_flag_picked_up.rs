9d23a3ad1e2816ccec764aae579a663ad2f6ee79
Tim Irmler <zwazel@hotmail.com>
2025-03-08 03:08:04 +0100

    if in base or not

--- server/src/gameplay/capture_the_flag/handle_flag_picked_up.rs
+++ server/src/gameplay/capture_the_flag/handle_flag_picked_up.rs
@@ -1,18 +1,28 @@
 use bevy::prelude::*;
-use shared::game::{collision_handling::components::CollisionLayer, flag::FlagState};
+use shared::game::{
+    collision_handling::components::CollisionLayer,
+    flag::{FlagBaseMarker, FlagMarker, FlagState},
+};
 
 use super::triggers::FlagGotPickedUpTrigger;
 
 // TODO: Send out network message for flag picked up(?)
 pub fn flag_picked_up(
     trigger: Trigger<FlagGotPickedUpTrigger>,
-    mut flags: Query<(&mut FlagState, &mut CollisionLayer)>,
+    mut flags: Query<(&mut FlagState, &mut CollisionLayer, &FlagMarker)>,
+    mut flag_bases: Query<(&mut CollisionLayer, &mut FlagBaseMarker), Without<FlagMarker>>,
 ) {
     let _lobby_entity = trigger.entity();
     let flag_entity = trigger.flag;
     let picker_entity = trigger.picker;
 
-    let (mut flag_state, mut collision_layer) = flags.get_mut(flag_entity).expect("Flag not found");
+    let (mut flag_state, mut collision_layer, flag_marker) =
+        flags.get_mut(flag_entity).expect("Flag not found");
+    let (mut _flag_base_collision_layer, mut flag_base_marker) = flag_bases
+        .get_mut(flag_marker.base)
+        .expect("Flag base not found");
+
+    flag_base_marker.flag_in_base = false;
 
     *flag_state = FlagState::Carried(picker_entity);
     *collision_layer = CollisionLayer::none(); // Can not collide with anything while carried
--- server/src/gameplay/capture_the_flag/reset_flags.rs
+++ server/src/gameplay/capture_the_flag/reset_flags.rs
@@ -6,6 +6,7 @@ use shared::game::{
 
 use super::triggers::ResetFlagTrigger;
 
+// TODO: Send out network message for flag reset(?)
 pub fn reset_flag(
     trigger: Trigger<ResetFlagTrigger>,
     mut flags: Query<
@@ -17,15 +18,18 @@ pub fn reset_flag(
         ),
         Without<FlagBaseMarker>,
     >,
-    bases: Query<&Transform, With<FlagBaseMarker>>,
+    mut bases: Query<(&mut FlagBaseMarker, &Transform)>,
 ) {
     let flag_entity = trigger.entity();
 
     let (mut wanted_transform, mut transform, flag_marker, mut flag_state) =
         flags.get_mut(flag_entity).expect("Flag not found");
 
-    let flag_base_transform = bases.get(flag_marker.base).expect("Flag base not found");
+    let (mut flag_base_marker, flag_base_transform) = bases
+        .get_mut(flag_marker.base)
+        .expect("Flag base not found");
 
+    flag_base_marker.flag_in_base = true;
     *wanted_transform = WantedTransform(*flag_base_transform);
     *transform = *flag_base_transform;
     *flag_state = FlagState::InBase;
