cf245e8a043954fb961d921bae9853a712baa6d2
Tim Irmler <zwazel@hotmail.com>
2025-03-09 16:33:27 +0100

    flag got dropped message

--- server/src/gameplay/capture_the_flag/handle_flag_dropped.rs
+++ server/src/gameplay/capture_the_flag/handle_flag_dropped.rs
@@ -1,18 +1,25 @@
 use bevy::prelude::*;
-use shared::game::{
-    collision_handling::components::CollisionLayer,
-    flag::{FlagCarrier, FlagMarker, FlagState},
+use shared::{
+    game::{
+        collision_handling::components::CollisionLayer,
+        flag::{FlagCarrier, FlagMarker, FlagState},
+    },
+    networking::messages::{
+        message_container::{MessageContainer, MessageTarget, NetworkMessageType},
+        message_data::flag_event_data::FlagEventDataWrapper,
+        message_queue::OutMessageQueue,
+    },
 };
 
-use super::triggers::FlagGotDroppedTrigger;
+use super::{triggers::FlagGotDroppedTrigger, MyLobby};
 
-// TODO: Send out network message for flag dropped(?)
 pub fn flag_dropped(
     trigger: Trigger<FlagGotDroppedTrigger>,
     mut flags: Query<(&mut FlagState, &mut CollisionLayer), With<FlagMarker>>,
+    mut lobby_queue: Query<&mut OutMessageQueue, With<MyLobby>>,
     mut commands: Commands,
 ) {
-    let _lobby_entity = trigger.entity();
+    let lobby_entity = trigger.entity();
     let flag_entity = trigger.flag;
     let carrier_entity = trigger.carrier;
 
@@ -21,6 +28,17 @@ pub fn flag_dropped(
     *flag_collision_layer = CollisionLayer::flag();
     flag_collision_layer.ignore.clear(); // Can be picked up by everyone again
     *flag_state = FlagState::Dropped;
+
+    let mut lobby_queue = lobby_queue
+        .get_mut(lobby_entity)
+        .expect("Message queue not found");
+    lobby_queue.push_back(MessageContainer::new(
+        MessageTarget::AllInLobby,
+        NetworkMessageType::FlagGotDropped(FlagEventDataWrapper {
+            flag_id: flag_entity,
+            carrier_id: carrier_entity,
+        }),
+    ));
 
     commands.entity(carrier_entity).remove::<FlagCarrier>();
 }
