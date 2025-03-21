f0e3d889a9d30bbe513fbbb97b6c833ba468aa8c
Tim Irmler <zwazel@hotmail.com>
2025-03-21 13:10:14 +0100

    added a logger?

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 use bevy::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut app = App::new();
 
     // if feature "server" is enabled
@@ -16,5 +16,15 @@ fn main() {
     #[cfg(feature = "spectator_client")]
     app.add_plugins((spectator_client::MySpectatorClientPlugin,));
 
+    // If server or client is enabled but not debug, add "fern" logger
+    #[cfg(any(feature = "server", feature = "spectator_client"))]
+    #[cfg(not(feature = "debug"))]
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Error)
+        .chain(fern::log_file("error.log")?)
+        .apply()?;
+
     app.run();
+
+    Ok(())
 }
